413010525070569ff2c38703206fead9b2c254a2
Cody <cdriver@hawaiil.edu>
2025-06-03 17:46:12 -1000

    2 states

--- icoboard/ver2/src/gpio_test.py
+++ icoboard/ver2/src/gpio_test.py
@@ -2,10 +2,11 @@ import RPi.GPIO as GPIO
 import time
 
 # Set pin numbering convention: BCM = GPIO number, BOARD = physical pin number
+# Enter "pinout" in bash to see diagram of connector
 GPIO.setmode(GPIO.BCM)
 #GPIO.setmode(GPIO.BOARD)
 
-# Set up GPIO pins
+# Set up GPIO pins, ONLY WORKS IN BCM MODE
 chan_list = [18,22,23,7,19,16,20,21,24]
 
 for i in chan_list:
@@ -16,7 +17,9 @@ def write_byte(value):
     for i,pin in enumerate(chan_list[:-1]):
         print(f"Shift {i}: {value>>i:08b} -> {value>>i&1}")
         GPIO.output(pin, value>>i&1)
+        print(f"Sending bit {i}...")
         if pin == chan_list[-2]:
+            print("Write bit sent...\n")
             GPIO.output(chan_list[-1], 1)
             GPIO.output(chan_list[-1], 0)
 
@@ -27,11 +30,11 @@ try:
     write_byte(0xFF)
     time.sleep(2)
     
-    write_byte(0x55)
-    time.sleep(2)
+#    write_byte(0x55)
+#    time.sleep(2)
 
-    write_byte(0xAA)
-    time.sleep(2)
+#    write_byte(0xAA)
+#   time.sleep(2)
 
 finally:
     GPIO.cleanup()
--- icoboard/ver2/src/matrix.v
+++ icoboard/ver2/src/matrix.v
@@ -3,19 +3,34 @@ module matrix(
 	input [0:7] RPI_IO,
 	input write_strobe,
 	output [0:63] output_pin,
-	output pmod1_1
+	output reg LED1,
+	output reg LED2
 );
+
+	reg [7:0] mem [0:1];
+	reg [1:0] counter = 2'b00;
+	reg toggle = 0;
+
 	always @(posedge clk_100mhz) begin
-		if (write_strobe) begin
-			output_pin[7:0] <= RPI_IO;
-			output_pin[15:8] <= RPI_IO;
-			output_pin[23:16] <= RPI_IO;
-			output_pin[31:24] <= RPI_IO;
-			output_pin[39:32] <= RPI_IO;
-			output_pin[47:40] <= RPI_IO;
-			output_pin[55:48] <= RPI_IO;
-			output_pin[63:56] <= RPI_IO;
+		if (write_strobe & counter < 2) begin
+			LED1 <= 0;
+			LED2 <= 0;
+			mem[counter] <= RPI_IO;
+			counter <= counter + 1;
 		end
+		if (counter == 2'b10) begin
+			LED1 <= 1;
+			LED2 <= 1;
+			counter <= 2'b00;
+		end
+		toggle <= ~toggle;
+		output_pin[7:0] <= mem[toggle];
+		output_pin[15:8] <= mem[toggle];
+		output_pin[23:16] <= mem[toggle];
+		output_pin[31:24] <= mem[toggle];
+		output_pin[39:32] <= mem[toggle];
+		output_pin[47:40] <= mem[toggle];
+		output_pin[55:48] <= mem[toggle];
+		output_pin[63:56] <= mem[toggle];
 	end
-	assign pmod1_1 = write_strobe;
 endmodule
