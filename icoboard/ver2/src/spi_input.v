bb8f531dfa48ba44d5b416597f75ce014ec32782
Cody <cdriver@hawaiil.edu>
2025-07-10 14:52:05 -1000

    implemented memory write&read

--- icoboard/ver2/src/spi_input.v
+++ icoboard/ver2/src/spi_input.v
@@ -30,20 +30,21 @@ module spi_input (
 			if (sync_clk_rising) begin
 				bit_count <= bit_count + 1;
 				byte_data_received <= {byte_data_received[6:0], MOSI_data};
-			end 
+			end
 		end
 	end
 
-	reg [7:0] byte_data_send = 8'b0;
-  reg [7:0] message = 8'b0;
-	wire ready_to_send = (bit_count == 4'b1000 && ~pi_clk);
+	reg [7:0] byte_data_send = 8'b0; reg [7:0] message = 8'b0; reg [3:0] mem [0:3];
+	wire ready_to_send = (bit_count == 4'b1000);
 	always @(posedge ico_clk) begin
 		if (~SEL_active) byte_data_send <= 8'b0;
 		else begin
-      if (ready_to_send) begin
-        byte_data_send <= byte_data_received;
-        message <= byte_data_received;
-      end
+			if (ready_to_send) begin
+				byte_data_send <= byte_data_received;
+				message <= byte_data_received;
+				if (byte_data_received[5:4] == 2'b00) mem[byte_data_received[7:6]] <= byte_data_received[3:0];
+				if (byte_data_received[5:4] == 2'b01) byte_data_send <= mem[byte_data_received[7:6]];
+			end
 			if (sync_clk_falling && bit_count >= 4'b1000) byte_data_send <= {byte_data_send[6:0], 1'b0};
 		end
 	end
@@ -53,9 +54,9 @@ module spi_input (
 	assign pmod_MOSI = MOSI;
 	assign pmod_MISO = MISO;
 	assign pmod_piclk = pi_clk;
-	assign rpi_io7 = message[0];
-	assign rpi_io8 = message[1];
-	assign rpi_io9 = message[2];
-	assign rpi_io10 = message[3];
+	assign rpi_io7 = mem[0][0];
+	assign rpi_io8 = mem[0][1];
+	assign rpi_io9 = mem[0][2];
+	assign rpi_io10 = mem[0][3];
 
 endmodule
